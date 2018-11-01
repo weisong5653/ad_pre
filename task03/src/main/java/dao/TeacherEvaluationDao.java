8e61c867bc5330f37dc0d629a0c6a256330e605b
李威松 <842785721@qq.com>
2018-11-01 23:40:36 +0800

    11.1 23:40

--- task03/src/main/java/dao/TeacherEvaluationDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entity.TeacherEvaluation;
-import entity.TeacherEvaluationRecords;
-import org.apache.ibatis.annotations.Param;
-
-public interface TeacherEvaluationDao {
-    public int createTeacherEvaluation(@Param("teacherEvaluationRecords") TeacherEvaluationRecords teacherEvaluationRecords);
-}
--- task03/src/main/java/exception/EvaluateExceptions.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class EvaluateExceptions extends RuntimeException {
-    public EvaluateExceptions(String message) {
-        super(message);
-    }
-}
